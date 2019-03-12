364926f777ada8186d8bf8c7c8d1342b20ec3eda
André Christofferson <andrechristofferson@hotmail.com>
2019-03-12 13:53:12 +0100

    Update StoreTime.java

--- src/store/time/StoreTime.java
+++ src/store/time/StoreTime.java
@@ -1,8 +1,3 @@
-/**
- * @author Nour Aldein Bahtite
- * 
- */
-
 package store.time;
 
 
@@ -10,23 +5,37 @@ public class StoreTime {
 	private ExponentialRandomStream randomExcept;
 	private UniformRandomStream randomUni;
 	
+	/**
+	 *  Construct a new Time Object
+	 * @param lambda
+	 * @param seed
+	 */
 
 	public StoreTime(double lambda, long seed) {
 		this.randomExcept = new ExponentialRandomStream(lambda, seed);
 		this.randomUni = new UniformRandomStream(lambda, seed);
 
 	}
-
+	/**
+	 * Returns time for next customer to arrive
+	 * @Return time for next customer
+	 */
 	public double timeNextCustomer() {
 		return randomExcept.next();
 
 	}
-
+	/**
+	 * Returns 
+	 * @return duration for pick event
+	 */
 	public double timeCustomerPick() {
 		return randomUni.next();
 
 	}
-
+	/**
+	 * 
+	 * @return time duration for checkout event
+	 */
 	public double timeCustomerCheckOut() {
 		return randomUni.next();
 	}
