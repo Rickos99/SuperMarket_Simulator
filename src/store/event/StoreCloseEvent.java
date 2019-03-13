30be72fa85fe6214dce9f8cf2324d357d6e3c623
noorbhtiti <43866697+noorbhtiti@users.noreply.github.com>
2019-03-13 13:52:52 +0100

    Update StoreCloseEvent.java

--- src/store/event/StoreCloseEvent.java
+++ src/store/event/StoreCloseEvent.java
@@ -3,6 +3,15 @@ package store.event;
 import simulator.Event;
 import store.state.StoreState;
 
+/**
+ * 
+ * @author Nour Aldein Bahtite
+ * @author Philip Eriksson
+ * @author Rickard Bemm
+ * @author André Christofferson
+ * 
+ */
+
 public class StoreCloseEvent extends Event {
 	
 public StoreCloseEvent(StoreState state, double time) {
