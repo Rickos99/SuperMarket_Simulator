41693d53a44976570060190894239d1a3b83cb46
Bernstgunnar <phieri-5@student.ltu.se>
2019-03-19 12:36:42 +0100

    adwadw

--- src/main/OptimizeTesting.java
+++ src/main/OptimizeTesting.java
@@ -31,32 +31,32 @@ public interface OptimizeTesting {
 //		  public static final double SIM_STOP_TIME = 999.0d;
 
 // Ex 3: (som sim2)
-// public static final int MAX_CUSTOMERS = 7;
-// public static final double ARRIVAL_SPEED = 3;
-//
-// public static final double MIN_PICKING_TIME = 0.6d;
-// public static final double MAX_PICKING_TIME = 0.9d;
-//
-// public static final double MIN_CHECKOUT_TIME = 0.35d;
-// public static final double MAX_CHECKOUT_TIME = 0.6d;
-//
-// public static final int TIME_SEED = 13;
-// public static final double TIME_STORE_CLOSE = 8.0d;
-// public static final double SIM_STOP_TIME = 999.0d;
+ public static final int MAX_CUSTOMERS = 7;
+ public static final double ARRIVAL_SPEED = 3;
+
+ public static final double MIN_PICKING_TIME = 0.6d;
+ public static final double MAX_PICKING_TIME = 0.9d;
+
+ public static final double MIN_CHECKOUT_TIME = 0.35d;
+ public static final double MAX_CHECKOUT_TIME = 0.6d;
+
+ public static final int TIME_SEED = 13;
+ public static final double TIME_STORE_CLOSE = 8.0d;
+ public static final double SIM_STOP_TIME = 999.0d;
 
 // Ex 4
-	    public static final int MAX_CUSTOMERS = 100;
-	    public static final double ARRIVAL_SPEED = 50;
-
-	    public static final double MIN_PICKING_TIME = 0.45d;
-	    public static final double MAX_PICKING_TIME = 0.65d;
-
-	    public static final double MIN_CHECKOUT_TIME = 0.2d;
-	    public static final double MAX_CHECKOUT_TIME = 0.3d;
-
-	    public static final int TIME_SEED = 42;
-	    public static final double TIME_STORE_CLOSE = 20.0d;
-	    public static final double SIM_STOP_TIME = 999.0d;
+//	    public static final int MAX_CUSTOMERS = 100;
+//	    public static final double ARRIVAL_SPEED = 50;
+//
+//	    public static final double MIN_PICKING_TIME = 0.45d;
+//	    public static final double MAX_PICKING_TIME = 0.65d;
+//
+//	    public static final double MIN_CHECKOUT_TIME = 0.2d;
+//	    public static final double MAX_CHECKOUT_TIME = 0.3d;
+//
+//	    public static final int TIME_SEED = 42;
+//	    public static final double TIME_STORE_CLOSE = 20.0d;
+//	    public static final double SIM_STOP_TIME = 999.0d;
 
 // Ex 5
 //
